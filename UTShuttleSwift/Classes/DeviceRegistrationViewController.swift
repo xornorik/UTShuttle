5587b935f82e82ac14ba5ef71cf4fab71b93348e
Srinivas Vemuri <iosdeveloper@admin-imac.local>
2017-07-10 12:15:01 +0530

    handled keyboard hiding

--- UTShuttleSwift/Classes/DeviceRegistrationViewController.swift
+++ UTShuttleSwift/Classes/DeviceRegistrationViewController.swift
@@ -2,7 +2,7 @@
 //  DeviceRegistrationViewController.swift
 //  UTShuttleSwift
 //
-//  Created by Apple Developer on 10/07/17.
+//  Created by Srinivas Vemuri on 10/07/17.
 //  Copyright © 2017 PLEXITECH. All rights reserved.
 //
 
@@ -41,5 +41,51 @@ class DeviceRegistrationViewController: UIViewController {
         nextButton.cornerRadius = 10
         textMessageButton.cornerRadius = 10
         
+        phoneNoTextField.delegate = self
+        
+        //setting gesture recognizer for dismissing keyboard
+        let tapGC = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        self.view.addGestureRecognizer(tapGC)
+    }
+    
+    func dismissKeyboard()
+    {
+        self.view.endEditing(false)
+    }
+}
+extension DeviceRegistrationViewController:UITextFieldDelegate
+{
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField == phoneNoTextField
+        {
+            moveTFUp(textField: textField)
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextFieldDidEndEditingReason) {
+        if textField == phoneNoTextField
+        {
+            moveTFDown(textField: textField)
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func moveTFUp(textField:UITextField)
+    {
+        UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseOut, animations: {
+            self.view.frame.origin.y -= 216
+        }, completion: nil)
+        
+    }
+    
+    func moveTFDown(textField:UITextField)
+    {
+        UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseOut, animations: {
+            self.view.frame.origin.y += 216
+        }, completion: nil)
     }
 }
